b19338308f1c11b134a8adeb771b0ddb648eb85a
8394-droid <101510060+8394-droid@users.noreply.github.com>
2022-07-03 22:00:38 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+/*alert("welcome to online busines calculator")*/
+var x = 150/40
+    var y = "the distance will take"    
+    var h = "hours to cover the distance, which is equivalent to "
+    var s = 150/40*60
+    var e = "minute"
+    console.log(y+' '+ x+h+' '+ s+e);
